167b72b793fb22e4ff0abb971808ea757ee752f0
JakubJurkian <kuba.jur03@gmail.com>
2026-01-09 13:00:08 +0100

    feat(database): implement EF Core with SQLite and SqlDeviceRepository

--- backend/src/SmartHome.Api/Program.cs
+++ backend/src/SmartHome.Api/Program.cs
@@ -1,6 +1,9 @@
 using SmartHome.Domain.Interfaces;
 using SmartHome.Infrastructure.Persistence;
 
+using Microsoft.EntityFrameworkCore;
+using SmartHome.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // SERVICES SECTION (DI Container)
@@ -13,11 +16,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Konfiguracja Bazy Danych
+builder.Services.AddDbContext<SmartHomeDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // REPOSITORY REGISTRATION
 // AddSingleton because we store data in memory (RAM)
 // for a database, we use AddScoped
 // Singleton ensures data persists across different requests
-builder.Services.AddSingleton<IDeviceRepository, InMemoryDeviceRepository>();
+// builder.Services.AddSingleton<IDeviceRepository, InMemoryDeviceRepository>();
+
+// We changed AddSingleton to AddScoped. DB lives shortly (for request)
+builder.Services.AddScoped<IDeviceRepository, SqlDeviceRepository>();
 
 var app = builder.Build();
 
@@ -25,7 +35,8 @@ var app = builder.Build();
 // Here we define the request handling pipeline
 
 // Enable Swagger UI only in Dev env.
-if (app.Environment.IsDevelopment()) {
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI(); // Generates the interactive HTML page
 }
